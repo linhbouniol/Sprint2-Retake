1835e448cc785447e11338b95acf2d9c4fda0df8
Linh Bouniol <paulalinh89@gmail.com>
2018-10-20 10:20:08 -0700

    Add custom cell

--- Sprint2-Retake/Views/ShoppingItemCollectionViewCell.swift
+++ Sprint2-Retake/Views/ShoppingItemCollectionViewCell.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ShoppingItemCollectionViewCell: UICollectionViewCell {
     
+    var shoppingItem: ShoppingItem? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBOutlet weak var addedNotAddedLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
-    
+    private func updateViews() {
+        guard let shoppingItem = shoppingItem, let image = shoppingItem.image else { return }
+        
+        nameLabel?.text = shoppingItem.name
+        imageView?.image = UIImage(named: image)
+        
+        if shoppingItem.isAdded == true {
+            addedNotAddedLabel.text = "Added"
+        } else {
+            addedNotAddedLabel.text = "Not Added"
+        }
+    }
 }
