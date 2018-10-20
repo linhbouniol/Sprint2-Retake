bee7e964f02715e6b696b9a00dd899f4eff946d2
Linh Bouniol <paulalinh89@gmail.com>
2018-10-20 10:12:15 -0700

    Add model controller

--- Sprint2-Retake/Model Controller/ShoppingItemController.swift
+++ Sprint2-Retake/Model Controller/ShoppingItemController.swift
@@ -38,5 +38,41 @@ class ShoppingItemController {
         }
     }
     
+    func update(shoppingItem: ShoppingItem, name: String, image: String, isAdded: Bool) {
+        guard let index = shoppingItems.index(of: shoppingItem) else { return }
+        
+        shoppingItems[index].name = name
+        shoppingItems[index].image = image
+        shoppingItems[index].isAdded = isAdded
+        
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            NSLog("Error saving to core data: \(error)")
+        }
+    }
     
+    func delete(shoppingItem: ShoppingItem) {
+        guard let index = shoppingItems.index(of: shoppingItem) else { return }
+        
+        shoppingItems.remove(at: index)
+        
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            NSLog("Error saving to core data: \(error)")
+        }
+    }
+   
+    func updateIsAdded(for shoppingItem: ShoppingItem) {
+        guard let index = shoppingItems.index(of: shoppingItem) else { return }
+        
+        shoppingItems[index].isAdded = !shoppingItems[index].isAdded
+        
+        do {
+            try CoreDataStack.shared.save()
+        } catch {
+            NSLog("Error saving to core data: \(error)")
+        }
+    }
 }
